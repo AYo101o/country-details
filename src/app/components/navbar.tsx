b1697b18c89fa52db1c11becaccf18739e78cf3c
AY_o101o <ayoakinniyi10@gmail.com>
2025-03-30 20:36:01 +0000

    2nd update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,10 @@
-import { SunMoon, Search } from "lucide-react";
+import { SunMoon, Globe, ListFilter } from "lucide-react";
+
 
 export default function Navbar() {
+
+  const language = "EN";
+
   return (
     <div className="flex flex-col text-white">
       <nav className="flex justify-between mb-5">
@@ -8,6 +12,10 @@ export default function Navbar() {
         <SunMoon />
       </nav>
         <input type="text" className="rounded-md border border-white p-3 m-3" placeholder="Search Countries ..."/>
+        <div className="flex justify-between m-5">
+          <button className="flex bg-gray-900 p-3 rounded-3xl"><span className="mr-3"><Globe/></span> {language}</button>
+          <button className="flex bg-gray-900 p-3 rounded-3xl"><span className="mr-3"><ListFilter/></span> Filter</button>
+        </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div className="p-3 bg-gray-900 w-full min-h-screen">
+    <div className="p-3 bg-gray-950 w-full min-h-screen">
       <Navbar/>
+
     </div>
   );
 }
